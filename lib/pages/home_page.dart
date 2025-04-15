6a908bbdb654a688a6dfbeef8f6af60fba91951b
codecinn27 <codecinnpro@gmail.com>
2025-04-15 18:22:58 +0800

    update add notes

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:p3_simple_crud_firebase/services/firestore.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,12 +9,43 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  //firestore 
+  final FirestoreService firestoreService = FirestoreService();
+
+  final TextEditingController textController = TextEditingController();
+  
+ void openNoteBox(){
+  showDialog(context: context, builder: (context)=> AlertDialog(
+    content: TextField(
+      controller: textController,
+    ),
+    actions:[
+      //button to save
+      ElevatedButton(
+        onPressed: (){
+          //add a new notes
+          firestoreService.addNote(textController.text);
+
+          //clear the controller
+          textController.clear();
+
+          //close the box
+          Navigator.pop(context);
+        },
+        child: Text("Add")
+      )
+    ]
+  ));
+
+ }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Notes")),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
+        onPressed: openNoteBox,
         child: const Icon(Icons.add),  
       ),
     );
